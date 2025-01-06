09d48ef8790b1d5780a40d6180c8eb9e4904845a
sumitpo <2797366715@qq.com>
2025-01-06 09:18:10 +0800

    feat: add sample cc project with gtest

---
+++ ccProject/test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+// Simple test case
+TEST(ExampleTest, SimpleAssertion) {
+  EXPECT_EQ(1 + 1, 2);  // Simple test
+}
+
+// Another test case
+TEST(ExampleTest, AnotherTest) {
+  EXPECT_TRUE(5 > 3);   // Another simple assertion
+}
---
+++ ccProject/x.sh
@@ -0,0 +1,5 @@
+full_clean() {
+  test -d ./build && rm -rf build
+}
+test ! -d ./build && mkdir build
+(cd build; cmake ..; make)
